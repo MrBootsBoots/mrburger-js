09721f44fb58bebd600f99a1c85544cd60884dd0
ilia <ilia.kniazev1@gmail.com>
2019-02-10 23:40:56 +0300

    commits

---
+++ js/hamburger-menu.js
@@ -0,0 +1,5 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-link');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenuItem = document.querySelectorAll('#nav__item');
+const hamburgerCloseBtn = document.querySelector('#close-hamburger');
+const hamburgerLogo = document.querySelector('#logo__link');
