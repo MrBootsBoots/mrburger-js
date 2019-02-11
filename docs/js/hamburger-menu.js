d4cfad2b00ce8382019d083dc648423eee1f4278
ilia <ilia.kniazev1@gmail.com>
2019-02-11 14:59:57 +0300

    add

---
+++ docs/js/hamburger-menu.js
@@ -0,0 +1,29 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-link');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenuItem = document.querySelectorAll('#nav__item');
+const hamburgerCloseBtn = document.querySelector('#close-hamburger');
+const hamburgerLogo = document.querySelector('#logo__link');
+
+// hamburger-icon onclick
+hamburgerIcon.addEventListener('click', e => {
+  e.preventDefault();
+  hamburgerMenu.style.display = 'flex';
+  hamburgerIcon.style.display = 'none';
+  document.body.style.overflow = 'hidden';
+});
+
+//close-icon onclick
+hamburgerCloseBtn.addEventListener('click', e => {
+  e.preventDefault();
+  hamburgerMenu.style.display = '';
+  hamburgerIcon.style.display = '';
+  document.body.style.overflow = '';
+});
+
+//logo onclick close
+hamburgerLogo.addEventListener('click', e => {
+  e.preventDefault();
+  hamburgerMenu.style.display = '';
+  hamburgerIcon.style.display = '';
+  document.body.style.overflow = '';
+});
